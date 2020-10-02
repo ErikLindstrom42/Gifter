6415f940f1fb6be560f459a08f31d609f367e0bb
Erik Lindstrom <elindstrom42@gmail.com>
2020-10-02 11:42:43 -0500

    cleared cache and trying again

---
+++ Gifter/client/src/components/PostForm.js
@@ -0,0 +1,12 @@
+import React, { useContext, useEffect} from "react";
+import { Button, Form, FormGroup, Label, Input, FormText} from "reactstrap";
+
+const PostForm = () => {
+    return(
+        <Form>
+            <FormGroup>
+                
+            </FormGroup>
+        </Form>
+    )
+}
